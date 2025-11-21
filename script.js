9f9674c9e0b51d25a38fa9a8d94c61a13682909e
Majid Rasool <majigujjar11471@gmail.com>
2025-11-22 01:11:00 +0500

    added index.html, style.css, scrpit.js

---
+++ script.js
@@ -0,0 +1,45 @@
+let navbar = 
+document.querySelector('.navbar');
+navbar.addEventListener('click', function() {
+    console.log('Navbar loaded sucessfully');
+});
+let hero = 
+document.querySelector('.hero');
+hero.addEventListener('click', function() {
+    console.log('Hero section clicked');
+}); 
+ let trends = 
+ document.querySelector('.trends');
+ trends.addEventListener('click', function() {
+    console.log('Trends section clicked');
+ });
+ let movies = 
+ document.querySelector('.movies');
+ movies.addEventListener('click', function() {
+    console.log('Movies section clicked');
+ });
+ let award = 
+ document.querySelector('.award');
+ award.addEventListener('click', function() {
+    console.log('Award section clicked');
+ });
+  let series = 
+  document.querySelector('.series');
+  series.addEventListener('click', function() {
+    console.log('Series section clicked');
+  });
+  let price = 
+  document.querySelector('.price');
+  price.addEventListener('click', function() {
+    console.log('Price section clicked');
+  });
+  let continues = 
+  document.querySelector('.continues');
+  continues.addEventListener('click', function() {
+    console.log('Continues section clicked');
+  });
+  let footer = 
+  document.querySelector('.footer');
+  footer.addEventListener('click', function() {
+    console.log('Footer section clicked');
+  });
